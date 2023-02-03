24c46db35198925f3135adf1b3642f9f654013fe
xko0 <germain.rgb@hotmail.fr>
2023-02-03 17:35:55 +0100

    removed unused files and vercel configs

--- free-engine/src/components/navbar/Navbar.jsx
+++ free-engine/src/components/navbar/Navbar.jsx
@@ -19,9 +19,6 @@ function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
